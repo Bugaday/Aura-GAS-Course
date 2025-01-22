fbb759f1b663bb34ccba0e83b74a1ec9bbcab2c6
Bugaday <37881782+Bugaday@users.noreply.github.com>
2025-01-22 17:34:04 +0000

    Testing highlights

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -5,10 +5,14 @@
 
 void AAuraEnemy::HighlightActor()
 {
-
+	bHighlighted = true;
+	//DrawDebugSphere(GetWorld(),GetActorLocation(),100.0f,12,FColor::Yellow,false,-1,0.0f,1.0f);
+	UE_LOG(LogTemp,Warning,TEXT("Highlighting Actor!"));
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
-
+	bHighlighted = false;
+	//DrawDebugSphere(GetWorld(),GetActorLocation(),100.0f,12,FColor::Red,false,-1,0.0f,1.0f);
+	UE_LOG(LogTemp,Warning,TEXT("Unhighlighting Actor!"));
 }
